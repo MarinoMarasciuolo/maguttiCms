922261fa76cbc6d3e492d9cb7741930845980e20
Marco Asperti <marco.a@gfstudio.com>
2021-09-29 09:46:10 +0200

    Add name to admin route

--- routes/admin/index.php
+++ routes/admin/index.php
@@ -25,25 +25,27 @@ use App\maguttiCms\Admin\Controllers\Auth\ResetPasswordController;
 Route::group(['namespace' => 'Admin', 'middleware' => ['adminauth', 'setlocaleadmin']], function () {
 
     Route::get('/', [AdminPagesController::class, 'home'])->name('admin_dashboard');
-    Route::get('/list/{section?}/{sub?}', [AdminPagesController::class, 'lista'])->middleware(AdminRole::class);
-    Route::get('/view/{section}/{id?}/{type?}', [AdminPagesController::class, 'view'])->middleware(AdminRole::class);
-    Route::get('/create/{section}', [AdminPagesController::class, 'create'])->middleware(AdminRole::class);
-    Route::post('/create/{section}', [AdminPagesController::class, 'store'])->middleware(AdminStoreRole::class);
+    Route::get('/list/{section?}/{sub?}', [AdminPagesController::class, 'lista'])->middleware(AdminRole::class)->name('admin_list');
+    Route::get('/view/{section}/{id?}/{type?}', [AdminPagesController::class, 'view'])->middleware(AdminRole::class)->name('admin_view');
+    Route::get('/create/{section}', [AdminPagesController::class, 'create'])->middleware(AdminRole::class)->name('admin_create');
+    Route::post('/create/{section}', [AdminPagesController::class, 'store'])->middleware(AdminStoreRole::class)->name('admin_store');
 
     Route::get('/edit/{section}/{id?}/{type?}', [AdminPagesController::class, 'edit'])->middleware(AdminEditRole::class)->name('admin_edit');
-    Route::post('/edit/{section}/{id?}', [AdminPagesController::class, 'update'])->middleware(AdminStoreRole::class);
+    Route::post('/edit/{section}/{id?}', [AdminPagesController::class, 'update'])->middleware(AdminStoreRole::class)->name('admin_update');
 
-    Route::get('/file_view/{section}/{id}/{key}', [AdminPagesController::class, 'file_view']);
+    Route::get('/delete/{section}/{id?}', [AdminPagesController::class, 'destroy'])->middleware(AdminDeleteRole::class)->name('admin_destroy');
 
-    Route::get('/editmodal/{section}/{id?}/{type?}', [AdminPagesController::class, 'editmodal']);
-    Route::post('/editmodal/{section}/{id?}', [AdminPagesController::class, 'updatemodal']);
-    Route::get('/delete/{section}/{id?}', [AdminPagesController::class, 'destroy'])->middleware(AdminDeleteRole::class);
+    Route::get('/duplicate/{section}/{id?}/{type?}', [AdminPagesController::class, 'duplicate'])->name('admin_duplicate');
 
-    Route::get('/duplicate/{section}/{id?}/{type?}', [AdminPagesController::class, 'duplicate']);
+    Route::get('/file_view/{section}/{id}/{key}', [AdminPagesController::class, 'file_view'])->name('file_view');
+
+    Route::get('/editmodal/{section}/{id?}/{type?}', [AdminPagesController::class, 'editmodal'])->name('admin_modal_edit');
+    Route::post('/editmodal/{section}/{id?}', [AdminPagesController::class, 'updatemodal'])->name('admin_modal_update');
+
 
     Route::group(array('prefix' => 'impersonated', 'middleware' => ['adminimpersonate']), function () {
-        Route::get('/adminusers/{id?}', [AdminImpersonateController::class,'impersonateadmin']);
-        Route::get('/users/{id?}', [AdminImpersonateController::class,'impersonateuser']);
+        Route::get('/adminusers/{id?}', [AdminImpersonateController::class,'impersonateadmin'])->name('impersonate_admin');
+        Route::get('/users/{id?}', [AdminImpersonateController::class,'impersonateuser'])->name('impersonate_user');
     });
 
     /*
@@ -53,7 +55,7 @@ Route::group(['namespace' => 'Admin', 'middleware' => ['adminauth', 'setlocalead
     */
     Route::prefix('api')->group(base_path('routes/admin/api.php'));
 
-    Route::get('/exportlist/{section?}/{sub?}', [AdminExportController::class,'list']);
+    Route::get('/exportlist/{section?}/{sub?}', [AdminExportController::class,'list'])->name('admin_exportlist');
 });
 /*
 |--------------------------------------------------------------------------
